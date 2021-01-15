cd0da6c7e5edeeb692b56855ec19f3903c3f53d1
rpiquois <rpiquois@klab.com>
2021-01-15 20:26:19 +0100

    Clean license.

--- hdlSPU/rtl/NoiseUnit.v
+++ hdlSPU/rtl/NoiseUnit.v
@@ -2,9 +2,9 @@
 
 PS-FPGA Licenses (DUAL License GPLv2 and commercial license)
 
-This PS-FPGA source code is copyright © 2019 Romain PIQUOIS (Laxer3a) and licensed under the GNU General Public License v2.0, 
+This PS-FPGA source code is copyright © 2019 Romain PIQUOIS and licensed under the GNU General Public License v2.0, 
  and a commercial licensing option.
-If you wish to use the source code from PS-FPGA, email laxer3a@hotmail.com for commercial licensing.
+If you wish to use the source code from PS-FPGA, email laxer3a [at] hotmail [dot] com for commercial licensing.
 
 See LICENSE file.
 ---------------------------------------------------------------------------------------------------------------------- */
